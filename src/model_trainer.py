b0bbbec6dca844096443c3d56902a7e72791fa25
Harveer Dhillon <harveerdhillon91@gmail.com>
2025-07-28 23:16:43 -0400

    Added logistic regression model

--- src/model_trainer.py
+++ src/model_trainer.py
@@ -27,4 +27,13 @@ X = pd.get_dummies(X)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=20)
 
 
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
+
+y_prediction = model.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
+print("Classification Report:\n", classification_report(y_test, y_pred))
+
 
